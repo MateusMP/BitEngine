93d27fc7aee406dde4fabf16ed1f187d43652750
MateusMP <mateus.mp93@gmail.com>
2015-07-13 15:57:21 -0300

    Basic keyboard input system

--- include/bitengine/InputSystem.h
+++ include/bitengine/InputSystem.h
@@ -6,7 +6,45 @@
 #include "MessageChannel.h"
 #include "MessageType.h"
 
+#include <unordered_map>
+
 namespace BitEngine{
+
+class InputReceiver
+{
+	public:
+		enum class KeyMod : unsigned char{
+			FALSE = 0x0,
+
+			SHIFT = 0x0001,
+			CTRL = 0x0002,
+			ALT = 0x0004,
+			SUPER = 0x0008,
+
+			TRUE = 0x0080
+
+			// Example: KeyPress/Release with no modifiers
+			// 1000 0000 => x = TRUE
+			// Example: KeyPress/Release with no CTRL+ALT
+			// 1000 0110 => x = TRUE | CTRL | ALT
+
+			// Receiving the value:
+			// Ex1:
+			// if (x & KeyMod::TRUE) --> Doesn't care for modifiers
+			//
+			// Ex2:
+			// if (x & (KeyMod::TRUE | KeyMod::CTRL | KeyMod::ALT) ) --> Check for key + CTRL + ALT
+		};
+
+		void keyboardInput(int key, int scancode, int action, int mods);
+
+		KeyMod isKeyPressed(int key);
+		KeyMod keyReleased(int key);
+
+	private:
+		std::unordered_map<unsigned int, KeyMod> m_keyDown;
+		std::unordered_map<unsigned int, KeyMod> m_keyReleased;
+};
 
 class InputSystem
     : public System
@@ -21,8 +59,15 @@ class InputSystem
 
         void Message(const WindowCreated& wndcr);
 
+		// Will verify for the main window (first created)
+		InputReceiver::KeyMod isKeyPressed(int key);
+		// Will verify for the main window (first created)
+		InputReceiver::KeyMod keyReleased(int key);
+
     private:
         static void GlfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		
+		static std::unordered_map<GLFWwindow*, InputReceiver> inputReceivers;
 
 };
 
--- src/InputSystem.cpp
+++ src/InputSystem.cpp
@@ -2,8 +2,11 @@
 #include "InputSystem.h"
 
 #include "MessageChannel.h"
+#include "EngineLoggers.h"
 
 namespace BitEngine{
+
+std::unordered_map<GLFWwindow*, InputReceiver> InputSystem::inputReceivers;
 
 
 InputSystem::InputSystem()
@@ -15,33 +18,9 @@ InputSystem::InputSystem()
 InputSystem::~InputSystem(){
 }
 
-void InputSystem::Shutdown()
-{
-
-}
-
-void InputSystem::GlfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (action != GLFW_PRESS)
-        return;
-
-    switch (key)
-    {
-        case GLFW_KEY_SPACE:
-            glfwSetTime(0.0);
-            break;
-    }
-}
-
-void InputSystem::Message(const WindowCreated& wndcr)
-{
-    glfwSetKeyCallback(wndcr.window, GlfwKeyCallback);
-    printf("MENSAGEM RECEBIDA: WindowCreated\n");
-}
-
 bool InputSystem::Init()
 {
-    return true;
+	return true;
 }
 
 void InputSystem::Update()
@@ -49,5 +28,107 @@ void InputSystem::Update()
 
 }
 
+void InputSystem::Shutdown()
+{
+	for (auto it : inputReceivers)
+		glfwSetKeyCallback(it.first, nullptr);
+}
+
+void InputSystem::GlfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_UNKNOWN){
+		LOGTO(Warning) << "Unknown key: scancode: " << scancode << " Action: " << action << " Mods: " << mods << endlog;
+
+	}
+
+	LOGTO(Verbose) << "Key Input on window " << window << " key: " << key << " scancode: " << scancode << " Action: " << action << " Mods: " << mods << endlog;
+
+	auto it = inputReceivers.find(window);
+	if (it != inputReceivers.end()){
+		it->second.keyboardInput(key, scancode, action, mods);
+	} else {
+		LOGTO(Error) << "Invalid window input!" << endlog;
+	}
+}
+
+void InputSystem::Message(const WindowCreated& wndcr)
+{
+    glfwSetKeyCallback(wndcr.window, GlfwKeyCallback);
+    printf("MENSAGEM RECEBIDA: WindowCreated\n");
+
+	// Creates instance for this window
+	inputReceivers[wndcr.window];
+}
+
+InputReceiver::KeyMod InputSystem::isKeyPressed(int key)
+{
+	auto w = inputReceivers.begin();
+	if (w == inputReceivers.end())
+		return InputReceiver::KeyMod::FALSE;
+
+	return w->second.isKeyPressed(key);
+}
+
+InputReceiver::KeyMod InputSystem::keyReleased(int key)
+{
+	auto w = inputReceivers.begin();
+	if (w == inputReceivers.end())
+		return InputReceiver::KeyMod::FALSE;
+
+	return w->second.keyReleased(key);
+}
+
+
+void InputReceiver::keyboardInput(int key, int scancode, int action, int mods)
+{
+	switch (action){
+
+		case GLFW_REPEAT:
+			m_keyDown[key] = (KeyMod)(((unsigned char)KeyMod::TRUE) | (unsigned char)mods);
+			m_keyReleased[key] = KeyMod::FALSE;
+
+			LOGTO(Verbose) << "Key repeat" << endlog;
+		break;
+
+		case GLFW_PRESS:
+			m_keyDown[key] = (KeyMod)(((unsigned char)KeyMod::TRUE) | (unsigned char)mods);
+			m_keyReleased[key] = KeyMod::FALSE;
+
+			LOGTO(Verbose) << "Key down" << endlog;
+		break;
+
+		case GLFW_RELEASE:
+			m_keyReleased[key] = (KeyMod)(((unsigned char)KeyMod::TRUE) | (unsigned char)mods);
+			m_keyDown[key] = KeyMod::FALSE;
+
+			LOGTO(Verbose) << "Key Release" << endlog;
+		break;
+
+		default:
+			LOGTO(Warning) << "Invalid key action: " << action << endlog;
+		break;
+	}
+
+	LOGTO(Verbose) << "State down: " << (unsigned int)m_keyDown[key] << endlog;
+	LOGTO(Verbose) << "State up: " << (unsigned int)m_keyReleased[key] << endlog;
+}
+
+InputReceiver::KeyMod InputReceiver::isKeyPressed(int key)
+{
+	auto k = m_keyDown.find(key);
+	if (k != m_keyDown.end())
+		return k->second;
+
+	return KeyMod::FALSE;
+}
+
+InputReceiver::KeyMod InputReceiver::keyReleased(int key)
+{
+	auto k = m_keyReleased.find(key);
+	if (k != m_keyReleased.end())
+		return k->second;
+
+	return KeyMod::FALSE;
+}
 
 }
