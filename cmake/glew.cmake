c303d792f5325ef389165b8b6a69d1a85719f5bf
MateusMP <mateus.mp93@gmail.com>
2017-04-02 11:28:51 -0300

    Fix superbuild

--- cmake/glew.cmake
+++ cmake/glew.cmake
@@ -10,6 +10,14 @@ if (NEED_GLEW)
 		message(STATUS "GLEW_FOUND!")
 		add_custom_target(glew_dep)
 	else()	
+		
+		# Fix cmake build when not on MSVC
+		if (NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+			set(FORCE_MSVC_VER_FIX 0)
+		else()
+			set(FORCE_MSVC_VER_FIX ${MSVC_VERSION})
+		endif()
+	
 		ExternalProject_Add(glew_dep
 			PREFIX ${BE_BASE_DEPENDENCY_DIRECTORY}/glew
 			URL https://github.com/nigels-com/glew/releases/download/glew-2.0.0/glew-2.0.0.zip
@@ -17,8 +25,10 @@ if (NEED_GLEW)
 			SOURCE_SUBDIR build/cmake
 			BUILD_COMMAND $(MAKE) COMMAND $(MAKE) install
 			CMAKE_ARGS 
+				-DMSVC_VERSION=${FORCE_MSVC_VER_FIX}
 				-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
 				-DBUILD_UTILS=OFF
+				-DBUILD_SHARED_LIBS=OFF
 		)
 		message(STATUS "GLEW needs to be built")
 	endif()
