8dff3bb8a4d817eb8860fbbc9359aca06a72c54f
MateusMP <mateus.mp93@gmail.com>
2017-04-21 11:24:26 -0300

    bitengine install command

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -2,15 +2,20 @@ if (BUILD_TESTS)
 
     list(APPEND DEPENDENCIES googletest)
 
+    if (MSVC)
+        set(BUILD_GTEST_MSVC_WITH_CRT -Dgtest_force_shared_crt:INTERNAL=ON)
+    endif()
+
     ExternalProject_Add(googletest
         PREFIX ${BE_BASE_DEPENDENCY_DIRECTORY}/gtest
         GIT_REPOSITORY    https://github.com/google/googletest.git
         GIT_TAG           aa148eb2b7f70ede0eb10de34b6254826bfb34f4
-        CMAKE_ARGS		-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-                        -Dgtest_force_shared_crt:INTERNAL=ON
-                        -DBUILD_SHARED_LIBS=OFF
-        TEST_COMMAND      ""
-        UPDATE_COMMAND ""
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+            ${BUILD_GTEST_MSVC_WITH_CRT}
+            -DBUILD_SHARED_LIBS=OFF
+        TEST_COMMAND    ""
+        UPDATE_COMMAND  ""
         # gtest_force_shared_crt ON CACHE BOOL "" FORCE
     )
     set(GTEST_INCLUDE ${CMAKE_INSTALL_PREFIX}/include)
