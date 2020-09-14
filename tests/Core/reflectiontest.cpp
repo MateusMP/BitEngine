5a7f8f2ca9d6f160e0a56d0b14b87b7e54caebd4
MateusMP <mateus.mp93@gmail.com>
2020-09-14 09:29:47 -0300

    Add a few more reflection tests

--- tests/Core/reflectiontest.cpp
+++ tests/Core/reflectiontest.cpp
@@ -11,6 +11,7 @@ class MyClass
 public:
         int x;
         float y;
+        int* ptr;
 };
 
 class Type2
@@ -34,9 +35,17 @@ DefineToStringFor(Type2, {
     return s.str();
 })
 
-// Override default int ToString to always return "INT".
+// Override default int ToString to format "int(value)".
 // Just an example!!
-DefineToStringFor(int, return "INT")
+DefineToStringFor(int, {
+    std::stringstream s;
+    if (value) {
+        s << "int(" << *typedValue << ")";
+    } else {
+        s << "int(NULL)";
+    }
+    return s.str(); 
+})
 
 // Reflection code
 REFLECT_START(MyClass)
@@ -55,6 +64,40 @@ TEST(ReflectionTest, ReflectedClassNameProperlyDefined)
 {
     ASSERT_EQ("MyClass", BitEngine::ClassName<MyClass>::Get());
     ASSERT_EQ("OtherClass", BitEngine::ClassName<OtherClass>::Get());
+}
+
+TEST(ReflectionTest, CustomToString)
+{
+    int myInt = 7;
+    std::string str = BitEngine::Reflection::TypeToString<int>::ToString(&myInt);
+    ASSERT_EQ(str, "int(7)"); // Was overriden above
+
+    unsigned int myUint = 7;
+    str = BitEngine::Reflection::TypeToString<unsigned int>::ToString(&myUint);
+    ASSERT_EQ(str, "7");
+
+    int* someIntPtr = (int*)~0;
+    str = BitEngine::Reflection::TypeToString<int*>::ToString(&someIntPtr);
+    ASSERT_TRUE(std::all_of(str.begin(), str.end(), [](char i) { return i == 'F'; })); // Independent of arch size
+
+    Type2 type2;
+    str = BitEngine::Reflection::TypeToString<Type2>::ToString(&type2);
+    std::stringstream expectedType2;
+    expectedType2 << "<" << &type2 << ">";
+    ASSERT_EQ(str, expectedType2.str());
+}
+
+TEST(ReflectionTest, Jsonize)
+{
+    MyClass myClass;
+    myClass.x = 5;
+    myClass.y = 117.68f;
+    myClass.ptr = (int*)0xff; // not reflected!!
+
+    BitEngine::Reflection::Reflected reflected = BitEngine::Reflection::Class::FromInstance(myClass);
+
+    std::string json = reflected.jsonize();
+    ASSERT_EQ(json, "{x: int(5),y: 117.68}"); // ptr is not reflected!
 }
 
 TEST(ReflectionTest, GetDataFromReflectedInstance)
@@ -78,4 +121,6 @@ TEST(ReflectionTest, SetDataInReflectedInstance)
     reflected.set("y", 677.22f);
     ASSERT_EQ(5, reflected.get<int>("x"));
     ASSERT_EQ(677.22f, reflected.get<float>("y"));
+    ASSERT_EQ(5, myClass.x);
+    ASSERT_EQ(677.22f, myClass.y);
 }
